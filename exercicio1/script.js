882b3ad7bd57c4b295abc9866cbebb2f05ee9561
francinaldapmfaria <portela854@gmail.com>
2022-08-29 17:22:10 -0300

    exercicio 1 realizado com sucesso

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// let nome
+// let idade
+// console.log(nome,idade)
+
+let nome = prompt('Qual é seu nome?')
+let idade = Number(prompt('Qual a sua idade'))
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`)
+console.log(typeof nome, typeof idade)
+
+
